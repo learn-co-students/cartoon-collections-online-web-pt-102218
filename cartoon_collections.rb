7dde49e03628e1e2361c4308a228cec23dacece7
A. V <anysiavalko@gmail.com>
2018-11-05 17:36:47 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,4 @@
+#accepts an array of dwarf names and puts them out with numbers
 def roll_call_dwarves(dwarves)
   new_array = [ ]
   dwarves.each_with_index do |dwarf, index|
@@ -6,6 +7,7 @@ def roll_call_dwarves(dwarves)
   return new_array
 end
 
+#Accept an array of calls and return a modified array where the calls are capitalized with an exclamation point
 def summon_captain_planet(planeteer_calls)
   new_array = [ ]
   planeteer_calls.collect do |element|
@@ -15,10 +17,12 @@ def summon_captain_planet(planeteer_calls)
 end
  #The method didn't pass when I did new_array.push(puts "#{element.capitalize}!"), I had to take out the 'puts'
 
+#Accept an array of calls and tell us if any calls are longer than four characters
 def long_planeteer_calls(words)
   words.any? {|word| word.length > 4}
 end
 
+#Accept an array of strings and through these strings to find and return the first string that is a type of cheese
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.detect {|snack| cheese_types.include?(snack)}
